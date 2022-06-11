1aeb3e194b381a524343562e1ba2bd033bd71377
Gabriel <j_gabrielorozco@live.com>
2022-06-11 17:14:30 -0500

    Commit inicial, se inicializa el viewBinding.

---
+++ app/src/main/java/com/example/appsmoviles_practica1_3/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appsmoviles_practica1_3
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appsmoviles_practica1_3.databinding.ActivityMainBinding
+
+private lateinit var mainBinding: ActivityMainBinding
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        with(mainBinding){
+            
+        }
+    }
+}
